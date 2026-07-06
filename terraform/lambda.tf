32bab752cbdd365c8ac0935e7dae9c470dfcdcec
Aase <aase@mit.edu>
2026-07-06 13:45:00 +0200

    terraform update to hancle batch and v2

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -213,5 +213,11 @@ resource "aws_s3_bucket_notification" "output_bucket_notification" {
     filter_prefix       = "v1/"
   }
 
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.trigger_handler_function.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "v2/"
+  }
+
   depends_on = [aws_lambda_permission.s3_invoke_trigger]
 }
